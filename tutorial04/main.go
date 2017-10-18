fc7015e7361e2312c369cb445db18a1ca96bb8d1
choo8 <choo8@illinois.edu>
2017-10-18 00:10:38 -0700

    Added tutorial 4 and fixed bug with tutorial 3 and 4 only rendering occasionally. Replaced gl.Strs() with gl.Str().

--- tutorial04/main.go
+++ tutorial04/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-gl/glfw/v3.2/glfw"
 	"github.com/go-gl/mathgl/mgl32"
 	"log"
+	"math/rand"
 	"runtime"
 )
 
@@ -32,7 +33,7 @@ func main() {
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 
 	// Open a window and create its OpenGL context
-	window, err := glfw.CreateWindow(1024, 768, "Tutorial 03 - Matrices", nil, nil)
+	window, err := glfw.CreateWindow(1024, 768, "Tutorial 04 - Colored Cube", nil, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,21 +45,23 @@ func main() {
 	// Dark blue background
 	gl.ClearColor(0.0, 0.0, 0.4, 0.0)
 
+	// Enable depth test
+	gl.Enable(gl.DEPTH_TEST)
+	// Accept fragment if it closer to the camera than the former one
+	gl.DepthFunc(gl.LESS)
+
 	var vertexArrayId uint32
 	gl.GenVertexArrays(1, &vertexArrayId)
 	gl.BindVertexArray(vertexArrayId)
 
 	// Create and compile our GLSL program from the shaders
-	programId, err := common.LoadShaders("SimpleTransform.vertexshader", "SingleColor.fragmentshader")
+	programId, err := common.LoadShaders("TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader")
 
 	// Get a handle for our "MVP" uniform
 	matrixId := gl.GetUniformLocation(programId, gl.Str("MVP"+"\x00"))
 
 	// Projection matrix : 45 degrees Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
 	projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(1024)/float32(768), 0.1, 100.0)
-	// Or, for an ortho camera :
-	// projection = mgl32.Ortho(-10.0, 10.0, -10.0, 10.0, 0.0, 100.0) // In world coordinates
-
 	// Camera matrix
 	view := mgl32.LookAtV(mgl32.Vec3{4, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
 	// Model matrix : an identity matrix (model will be at the origin)
@@ -66,10 +69,54 @@ func main() {
 	// Our ModelViewProjection : multiplication of our 3 matrices
 	MVP := projection.Mul4(view.Mul4(model))
 
+	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
 	gVertexBufferData := []float32{
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
 		-1.0, -1.0, 0.0,
 		1.0, -1.0, 0.0,
 		0.0, 1.0, 0.0,
+	}
+
+	// One color for each vertex. They were generated randomly.
+	var gColorBufferData []float32
+	for i := 0; i < len(gVertexBufferData); i++ {
+		gColorBufferData = append(gColorBufferData, rand.Float32())
 	}
 
 	var vertexBuffer uint32
@@ -77,9 +124,14 @@ func main() {
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
 	gl.BufferData(gl.ARRAY_BUFFER, len(gVertexBufferData)*4, gl.Ptr(gVertexBufferData), gl.STATIC_DRAW)
 
+	var colorBuffer uint32
+	gl.GenBuffers(1, &colorBuffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+	gl.BufferData(gl.ARRAY_BUFFER, len(gColorBufferData)*4, gl.Ptr(gColorBufferData), gl.STATIC_DRAW)
+
 	for window.GetKey(glfw.KeyEscape) != glfw.Press && !window.ShouldClose() {
 		// Clear the screen
-		gl.Clear(gl.COLOR_BUFFER_BIT)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
 		// Use our shader
 		gl.UseProgram(programId)
@@ -93,9 +145,15 @@ func main() {
 		gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
 		gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
 
+		// 2nd attribute buffer : colors
+		gl.EnableVertexAttribArray(1)
+		gl.BindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+		gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 0, nil)
+
 		// Draw the triangles!
-		gl.DrawArrays(gl.TRIANGLES, 0, 3) // 3 indices starting 0 -> 1 triangle
+		gl.DrawArrays(gl.TRIANGLES, 0, 12*3) // 3 indices starting 0 -> 1 triangle
 		gl.DisableVertexAttribArray(0)
+		gl.DisableVertexAttribArray(1)
 
 		// Swap buffers
 		window.SwapBuffers()
@@ -104,6 +162,7 @@ func main() {
 
 	// Cleanup VBO
 	gl.DeleteBuffers(1, &vertexBuffer)
+	gl.DeleteBuffers(1, &colorBuffer)
 	gl.DeleteProgram(programId)
 	gl.DeleteVertexArrays(1, &vertexArrayId)
 
